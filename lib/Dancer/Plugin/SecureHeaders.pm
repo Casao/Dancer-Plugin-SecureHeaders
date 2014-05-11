2814cd5a6f4139176eee69d7df75c16c18b72a03
Colin Ewen <colin@draecas.com>
2014-05-11 13:42:50 -0400

    Version 1.0.2

--- lib/Dancer/Plugin/SecureHeaders.pm
+++ lib/Dancer/Plugin/SecureHeaders.pm
@@ -12,7 +12,7 @@ Dancer::Plugin::SecureHeaders - Automate HTTP Security headers.
 
 =head1 VERSION
 
-Version 1.0.1
+Version 1.0.2
 
 =cut
 
@@ -20,7 +20,7 @@ Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.1';
+our $VERSION = '1.0.2';
 
 =head1 DESCRIPTION
 
